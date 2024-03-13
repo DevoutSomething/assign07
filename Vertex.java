5390353c181056a34131efe9cff9de33195d2334
DevoutSomething <71037555+DevoutSomething@users.noreply.github.com>
2024-03-12 22:17:50 -0700

    Add files via upload

--- Vertex.java
+++ Vertex.java
@@ -2,53 +2,113 @@ package assign07;
 
 import java.util.LinkedList;
 
-public class Vertex <T> {
+/**
+ * Vertex class to be used in Graph
+ * 
+ * @author Alex Dean and Archer Fox
+ * @version 3/12/2023
+ */
+public class Vertex<T> {
 
     private T data;
     private LinkedList<Edge<T>> adjacents;
     private int indegrees;
     private Vertex<T> visitedFrom;
-    private boolean visited; 
+    private boolean visited;
 
-
-    public boolean getVisited(){
-        return visited;
-    }
-    public void setVisited(boolean choice){
-        visited = choice;
-    }
-    public Vertex<T>  getVisitedFrom(){
-        return visitedFrom;
-    }
-    public void setVisitedFrom(Vertex<T> vertex){
-        visitedFrom = vertex;
-    }
+    /**
+     * Constructs a Vertex object
+     * 
+     * @param data
+     */
     public Vertex(T data) {
         this.data = data;
         this.adjacents = new LinkedList<>();
         this.indegrees = 0;
     }
 
+    /**
+     * Gets the visited value of the vertex
+     * 
+     * @return true if visited, false otherwise
+     */
+    public boolean getVisited() {
+        return visited;
+    }
+
+    /**
+     * Sets the visited value of the vertex
+     * 
+     * @param choice
+     */
+    public void setVisited(boolean choice) {
+        visited = choice;
+    }
+
+    /**
+     * Gets the vertex that this vertex was visited from
+     * 
+     * @return visitedFrom
+     */
+    public Vertex<T> getVisitedFrom() {
+        return visitedFrom;
+    }
+
+    /**
+     * Sets the vertex that this vertex was visited from
+     * 
+     * @param vertex
+     */
+    public void setVisitedFrom(Vertex<T> vertex) {
+        visitedFrom = vertex;
+    }
+
+    /**
+     * Gets the value of this vertex
+     * 
+     * @return data
+     */
     public T getVertexData() {
         return this.data;
     }
 
+    /**
+     * Gets the list of adjacent vertices
+     * 
+     * @return adjacency list
+     */
     public LinkedList<Edge<T>> getAdjacents() {
         return this.adjacents;
     }
 
+    /**
+     * Adds an edge to this vertex
+     * 
+     * @param other
+     */
     public void addEdge(Vertex<T> other) {
         this.adjacents.add(new Edge<T>(other));
     }
 
+    /**
+     * Gets the number of indegrees this vertex has
+     * 
+     * @return num of indegrees as an int
+     */
     public int getIndegrees() {
         return this.indegrees;
     }
 
+    /**
+     * Increments the amount of indegrees
+     */
     public void incrementIndegrees() {
         this.indegrees++;
     }
 
+    /**
+     * Decrements the amount of indegrees
+     */
     public void decrementIndegrees() {
         this.indegrees--;
     }
